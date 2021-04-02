785971981c7e9a7accf08059a79a4e00a948df60
Steven Lopes <slopes@heartcup.com>
2021-04-01 23:19:35 -0700

    - Adding basic registration endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(AuthRequest $request){
+        $user = User::create([
+           'name' => $request->name,
+           'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
