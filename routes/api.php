eb3af6d2d765779592affd23c20724c572bf5ee8
Steven Lopes <slopes@heartcup.com>
2021-04-02 23:47:16 -0700

    - Added register and login routes

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ use \App\Http\Controllers\AuthController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/test', [AuthController::class, 'test']);
