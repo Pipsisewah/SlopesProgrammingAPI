1569e5ba3c232b8466979625f0c539cf4a2af4ef
Steven Lopes <slopes@heartcup.com>
2021-04-02 23:37:58 -0700

    - Adding simple registration and login routes.  Also added a test route to check if auth is working.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request){
+        Log::notice("Received request to create user, " , ['info' => $request->toArray()]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function login(LoginRequest $request){
+        Log::notice("User attempting to login", ['info' => $request->toArray()]);
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken($request->device_name)->plainTextToken;
+    }
+
+    public function test(Request $request){
+        return response()->json(['message' => 'hello'],200);
+    }
+}
